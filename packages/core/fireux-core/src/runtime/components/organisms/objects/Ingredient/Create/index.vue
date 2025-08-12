030ed222412f2f3ed1d319bc97c37e3636182fe2
fire-uxxx <fire@fireux.app>
2025-08-12 07:06:44 +0200

    feat(ingredients): inline quick-add on page, duplicate guard, count meta, highlight new; remove obsolete create component

--- packages/core/fireux-core/src/runtime/components/organisms/objects/Ingredient/Create/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import IngredientCreateSystem from './System.vue'
-</script>
-
-<template>
-  <div class="objects create ingredient-create">
-    <IngredientCreateSystem />
-  </div>
-</template>
