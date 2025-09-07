e19566f0f9b9d37b7cc0c5c8fc643c583dbbb894
fire-uxxx <fire@fireux.app>
2025-09-08 00:23:18 +0200

    chore: purge unintended tracked JS under src/ and projects/fireux/tennant; keep TS sources only

--- packages/core/fireux-core/src/config/plugins-config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configurePlugins = configurePlugins;
-var kit_1 = require("@nuxt/kit");
-function configurePlugins(resolver) {
-    // Client/runtime plugin example; add more as needed
-    var maybeClient = resolver.resolve('./runtime/plugins/firebase.client.ts');
-    (0, kit_1.addPlugin)({ src: maybeClient, mode: 'client' });
-}
